2559b96b2b6bf7d629b33f7887173c7865e602ac
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-08-18 19:21:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,11 @@ import numpy as np
 
 st.title("Asta random")
 
-if 'random_seed' not in st.session_state:
-    st.session_state['random_seed'] = np.random.randint(0, 100)
-
 if 'i' not in st.session_state:
     st.session_state['i'] = 0
 
-    
 uploaded_file = st.file_uploader("Carica file excel con le quotazioni aggiornate", type=["xlsx"])
 dropped_lines_df = pd.DataFrame()
-random_seed = st.session_state['random_seed']
-np.random.seed(random_seed)
-st.write(str(random_seed) + " - Numero di emergenza")
 
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
@@ -24,9 +17,7 @@ if uploaded_file is not None:
     df.columns = df.iloc[0,:].to_list()
     df = df.iloc[1:,:]
     df = df[["RM",	"Nome",	"Squadra",	"Qt.A"]]
-    df = df[df["Qt.A"]>soglia_minima]
-    df = df.sample(frac=1)
-    df.reset_index(drop=True, inplace=True)
+    df = df[df["Qt.A"]>=soglia_minima]
     
     if st.button("Estrai un giocatore"):
         i = st.session_state['i']
