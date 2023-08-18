69936f04e08bae9357ce41f970a389f520d6ad9a
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-08-18 19:03:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import pandas as pd
 import openpyxl
-import numpy.random as random
+import numpy as np
 
 st.title("Asta random")
     
 uploaded_file = st.file_uploader("Carica file excel con le quotazioni aggiornate", type=["xlsx"])
 dropped_lines_df = pd.DataFrame()
-
+random_seed = random.randint(0, 100)
+st.write(str(random_seed) + " - Numero di emergenza")
+np.random.seed(random_seed)
+i = 0
 
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
@@ -16,12 +19,12 @@ if uploaded_file is not None:
     df = df.iloc[1:,:]
     df = df[["RM",	"Nome",	"Squadra",	"Qt.A"]]
     df = df[df["Qt.A"]>soglia_minima]
+    df = df.sample(frac=1)
+    df.reset_index(drop=True, inplace=True)
+    
     if st.button("Estrai un giocatore"):
-        random_index = random.randint(0, len(df) - 1)
-        random_line = df.iloc[random_index]
-        df.drop(index=random_line.name, inplace=True)
-        dropped_lines_df = dropped_lines_df.append(random_line)
-        st.write("Giocatori rimasti:", len(df))
-        st.write(dropped_lines_df)
+        st.write("Giocatori mancanti:", len(df)-i-1)
+        st.write(df.iloc[:i,:])
+
         
     
